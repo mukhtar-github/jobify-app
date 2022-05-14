4b85f0f69178cdc91fdb4ef6071679d2dac4cd51
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-14 21:11:09 +0100

    Update files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import Landing from "./pages/Landing";
+import styled from "styled-components";
 
 function App() {
   return (
     <div>
-      <h1>Jobify</h1>
       <Landing />
     </div>
   );
