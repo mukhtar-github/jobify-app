4afd42f0fd70963d4a12c6392cff71a47e60ab29
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-15 12:02:22 +0100

    Update changes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -20,7 +21,9 @@ const Landing = () => {
             kombucha seitan austin pok pok selfies bushwick. Jean shorts
             bushwick vinyl street art umami.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login / Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
