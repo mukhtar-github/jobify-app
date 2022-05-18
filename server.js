a59c8c6dd8c2d8bb6c7315b50333345decadd67a
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-18 09:30:18 +0100

    Update files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
 // Middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
@@ -15,6 +16,15 @@ app.get("/", (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
