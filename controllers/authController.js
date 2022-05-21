3b2427e0e4ff28176be5751c12036e30112991df
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-21 17:01:49 +0100

    Update file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,17 @@
-import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import User from "../models/User.js";
+import { BadRequestError } from "../errors/index.js";
 
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.StatusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.StatusCode = StatusCodes.NOT_FOUND;
-  }
-}
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all values");
   }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
