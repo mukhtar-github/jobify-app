6d57a03b566af586dc9822159888b69bc2e24014
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-21 16:38:09 +0100

    Update changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,14 +4,24 @@ import { StatusCodes } from "http-status-codes";
 class CustomAPIError extends Error {
   constructor(message) {
     super(message);
+  }
+}
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
     this.StatusCode = StatusCodes.BAD_REQUEST;
   }
 }
-
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.StatusCode = StatusCodes.NOT_FOUND;
+  }
+}
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new CustomAPIError("Please provide all values");
+    throw new BadRequestError("Please provide all values");
   }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
