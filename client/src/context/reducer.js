972e1753a63c3386d059b81d385428f5a8b748ff
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-27 04:29:09 +0100

    Update file

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -43,6 +43,16 @@ const reducer = (state, action) => {
     };
   }
 
+  if (action.type === REGISTER_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
+
   throw new Error(`no such action :${action.type}`);
 };
 export default reducer;
