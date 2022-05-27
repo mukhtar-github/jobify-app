5904991514c3ba3f7cf54f6db727927e1fa9e393
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-27 07:31:18 +0100

    Update file

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,4 +1,6 @@
 import React, { useReducer, useContext } from "react";
+import reducer from "./reducer";
+import axios from "axios";
 import {
   DISPLAY_ALERT,
   CLEAR_ALERT,
@@ -6,18 +8,20 @@ import {
   REGISTER_USER_SUCCESS,
   REGISTER_USER_ERROR,
 } from "./actions";
-import reducer from "./reducer";
-import axios from "axios";
+
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
 
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
-  userLocation: "",
-  jobLocation: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || "",
 };
 
 const AppContext = React.createContext();
@@ -40,6 +44,18 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("location");
+  };
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
@@ -55,12 +71,11 @@ const AppProvider = ({ children }) => {
         },
       });
 
-      // will add later
-      // addUserToLocalStorage({
-      //   user,
-      //   token,
-      //   location,
-      // })
+      addUserToLocalStorage({
+        user,
+        token,
+        location,
+      });
     } catch (error) {
       // console.log(error.response);
       dispatch({
