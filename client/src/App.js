1546e4401920c11578b091c5885fdabdbb36c6b1
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-29 14:22:37 +0100

    Update file

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>dashboard</div>} />
+        <Route path="/">
+          <Route path="stats" element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />}></Route>
+          <Route path="add-job" element={<AddJob />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
