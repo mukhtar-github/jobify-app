d81f5b3c827a7b2dea9cbfa7ac4817c62d1c7904
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-07 06:38:30 +0100

    Update changes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // Middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateUser from "./middleware/auth.js";
 
 const app = express();
 
@@ -33,7 +34,7 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
