e188e94996e17a35a1f6c806c2709e5141cc6aee
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-15 06:27:56 +0100

    Update file

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -150,6 +150,10 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
+  const updateUser = async (currentUser) => {
+    console.log(currentUser);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -160,6 +164,7 @@ const AppProvider = ({ children }) => {
         setupUser,
         logoutUser,
         toggleSidebar,
+        updateUser,
       }}
     >
       {children}
