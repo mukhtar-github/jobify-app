48395868e728f8d7d006280c6b1c85da0172d7bd
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-26 16:52:52 +0100

    Update files

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -43,7 +43,7 @@ const AppProvider = ({ children }) => {
   // request
   authFetch.interceptors.request.use(
     (config) => {
-      // config.headers.common["Authorization"] = `Bearer ${state.token}`;
+      config.headers.common["Authorization"] = `Bearer ${state.token}`;
       return config;
     },
     (error) => {
@@ -57,7 +57,7 @@ const AppProvider = ({ children }) => {
       return response;
     },
     (error) => {
-      console.log(error.response);
+      // console.log(error.response);
       if (error.response.status === 401) {
         logoutUser();
       }
