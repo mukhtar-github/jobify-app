834bf70aa54ea0381b2cfbbfb5940ec7bd023ac6
Mukhtar <mukhtartanimugarba@gmail.com>
2022-07-06 09:31:02 +0100

    Update file

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -20,7 +20,11 @@ const deleteJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  return res.send("get all job");
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 
 const updateJob = async (req, res) => {
