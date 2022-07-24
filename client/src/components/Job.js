a3010fee3177c90864743201a19c9405422134bf
Mukhtar <mukhtartanimugarba@gmail.com>
2022-07-24 12:09:00 +0100

    Update files

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -15,13 +15,20 @@ const Job = ({
   createdAt,
   status,
 }) => {
+  const { setEditJob, deleteJob } = useAppContext();
+
   let date = moment(createdAt);
   date = date.format("MMM Do, YYYY");
   return (
-    <div>
-      <h5>{company}</h5>
-      <h5>{date}</h5>
-    </div>
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+    </Wrapper>
   );
 };
 
