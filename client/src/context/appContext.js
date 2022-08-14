8f5c970e4ee9c8e1789f467f4fc39f9f2660186c
Mukhtar <mukhtartanimugarba@gmail.com>
2022-08-14 17:44:52 +0100

    Update files

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -26,6 +26,7 @@ import {
   EDIT_JOB_ERROR,
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
+  CLEAR_FILTERS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -302,7 +303,7 @@ const AppProvider = ({ children }) => {
   };
 
   const clearFilters = () => {
-    console.log("clear filters");
+    dispatch({ type: CLEAR_FILTERS });
   };
 
   return (
