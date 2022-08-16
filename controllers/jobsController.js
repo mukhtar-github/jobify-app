3f13bd9521b5c0f204d97b23e7e6394be8347eb8
Mukhtar <mukhtartanimugarba@gmail.com>
2022-08-16 08:56:52 +0100

    Update files

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -19,28 +19,21 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  // const jobs = await Job.find({ createdBy: req.user.userId });
   const { search, status, jobType, sort } = req.query;
-
   const queryObject = {
     createdBy: req.user.userId,
   };
-
-  // add stuff based on condition
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
   if (status !== "all") {
     queryObject.status = status;
   }
   if (jobType !== "all") {
     queryObject.jobType = jobType;
   }
-  if (search) {
-    queryObject.position = { $regex: search, $options: "i" };
-  }
-
-  // NO AWAIT
   let result = Job.find(queryObject);
 
-  // chain sort conditions
   if (sort === "latest") {
     result = result.sort("-createdAt");
   }
@@ -54,11 +47,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
