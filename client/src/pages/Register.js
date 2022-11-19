583b13784f391e1d3b8ccd5d32378c8ebf36ead1
Mukhtar <mukhtartanimugarba@gmail.com>
2022-11-19 16:52:18 +0100

    Update file

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -104,6 +104,20 @@ function Register() {
         <button type="submit" className="btn btn-block" disabled={isLoading}>
           submit
         </button>
+        <button
+          type="button"
+          className="btn btn-block btn-hipster"
+          disabled={isLoading}
+          onClick={() => {
+            setupUser({
+              currentUser: { email: "testUser@test.com", password: "secret" },
+              endPoint: "login",
+              alertText: "Login Successful! Redirecting...",
+            });
+          }}
+        >
+          {isLoading ? "loading..." : "demo app"}
+        </button>
         <p>
           {values.isMember ? "Not a member yet?" : "Already a member?"}
           <button type="button" onClick={toggleMember} className="member-btn">
