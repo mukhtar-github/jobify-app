6380ce505208a1efca6311066315e10de9639f64
Mukhtar <mukhtartanimugarba@gmail.com>
2022-11-27 10:52:26 +0100

    Update file

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,6 +42,14 @@ const login = async (req, res) => {
   }
   const token = user.createJWT();
   user.password = undefined;
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
