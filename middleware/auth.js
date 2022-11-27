239d25d20bc59f6e770ceb66cd1cb7788b40d0f9
Mukhtar <mukhtartanimugarba@gmail.com>
2022-11-27 14:33:54 +0100

    Update file

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,15 +2,17 @@ import jwt from "jsonwebtoken";
 import { UnAuthenticatedError } from "../errors/index.js";
 
 const auth = async (req, res, next) => {
-  console.log(req.cookies);
-  // check header
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw new UnAuthenticatedError("Authentication invalid");
+  const token = req.cookies.token;
+  if (!token) {
+    throw new UnAuthenticatedError("Authentication Invalid");
   }
 
-  const token = authHeader.split(" ")[1];
+  // // check header
+  // const authHeader = req.headers.authorization;
+  // if (!authHeader || !authHeader.startsWith("Bearer")) {
+  //   throw new UnAuthenticatedError("Authentication invalid");
+  // }
+  // const token = authHeader.split(" ")[1];
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
