8639e2f8bb5edad3c2ca62e9ad8fc1751b7035ed
Mukhtar <mukhtartanimugarba@gmail.com>
2022-12-18 20:32:02 +0100

    Update file

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,32 +2,37 @@ import React from "react";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
 
 const Landing = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Logo />
-      </nav>
-      <div className="container page">
-        {/* info */}
-        <div className="info">
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>
-            I'm baby cold-pressed cray adaptogen, ramps wolf tattooed leggings
-            kombucha seitan austin pok pok selfies bushwick. Jean shorts
-            bushwick vinyl street art umami.
-          </p>
-          <Link to="/register" className="btn btn-hero">
-            Login / Register
-          </Link>
+    <React.Fragment>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              I'm baby cold-pressed cray adaptogen, ramps wolf tattooed leggings
+              kombucha seitan austin pok pok selfies bushwick. Jean shorts
+              bushwick vinyl street art umami.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login / Register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </React.Fragment>
   );
 };
 
