227d723955c953e9b097af543cc383834ccea86a
Mukhtar <mukhtartanimugarba@gmail.com>
2022-12-18 17:23:51 +0100

    Update file

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -242,6 +242,26 @@ const reducer = (state, action) => {
     return { ...state, page: action.payload.page };
   }
 
+  if (action.type === GET_CURRENT_USER_BEGIN) {
+    return { ...state, userLoading: true, showAlert: false };
+  }
+  if (action.type === GET_CURRENT_USER_SUCCESS) {
+    return {
+      ...state,
+      userLoading: false,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+    };
+  }
+
+  if (action.type === LOGOUT_USER) {
+    return {
+      ...initialState,
+      userLoading: false,
+    };
+  }
+
   throw new Error(`no such action :${action.type}`);
 };
 
