79a5994fd10eefc7b868c88dc95f1d6ba687b3a4
Mukhtar <mukhtartanimugarba@gmail.com>
2022-12-19 07:31:35 +0100

    Update file

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,7 +83,7 @@ const getCurrentUser = async (req, res) => {
 const logout = async (req, res) => {
   res.cookie("token", "logout", {
     httpOnly: true,
-    expires: new Date(Date.now() + 1000),
+    expires: new Date(Date.now()),
   });
   res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
